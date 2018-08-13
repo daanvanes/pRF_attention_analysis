fe80fbdf15b80ae7b69065c28b3346755b5475be
Daan <daan.van.es@gmail.com>
2018-08-13 16:46:04 +0200

    removed trash and pruned scripts

--- pruned_scripts/cancel_jobs.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-from IPython import embed as shell
-import numpy as np
-
-os.system('cd ~/job_submissions/')
-
-for jobno in np.arange(int(sys.argv[1]),int(sys.argv[2])+1,1):
-	os.system('scancel ' + str(jobno))
